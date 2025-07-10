13832e8784d439bfd1e92825e7da9aa9748f5db0
Vepa Babayev <vepababayev03@gmail.com>
2025-07-10 18:13:21 +0200

    added login page

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:login_verif/pages/LoginPage.dart';
 import 'package:login_verif/pages/otp.dart';
 
 class HomePage extends StatefulWidget {
@@ -71,6 +72,12 @@ class _HomePageState extends State<HomePage> {
                 hintStyle: TextStyle(fontSize: 20, color: Colors.grey),
               ),
             ),
+            SizedBox(height: 10),
+            GestureDetector(
+              onTap: (){
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Loginpage()));
+              },
+              child: Text("Already registered click")),
             SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,86 @@
+import 'dart:convert' show jsonEncode, jsonDecode;
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:login_verif/pages/MainPage.dart' show Mainpage;
+
+class Loginpage extends StatefulWidget {
+  const Loginpage({super.key});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  TextEditingController MailController = TextEditingController();
+  TextEditingController PasswordController = TextEditingController();
+
+  Future<void> login_verif() async {
+    final url = Uri.parse('http://192.168.100.33:8000/login/');
+    final response = await http.post(
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      'email': MailController.text.trim(),
+      'password': PasswordController.text,
+    }),
+  );
+    final responseBody = jsonDecode(response.body);
+    print('Response body: $responseBody');
+
+    // Status kodu kontrol etmek yerine mesajı kontrol et
+    if (responseBody['message'] == 'Login successful') {
+      print('✅ Login succesfully');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Mainpage()),
+      );
+    } if (responseBody['message'] == 'User not found') {
+      print('User not found');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('User not found')),
+      );
+    } if (responseBody['message'] == 'Account not verified') {
+      print(' Account not verified');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Account not verifiedt')),
+      );
+    } if (responseBody['message'] == 'Invalid password') {
+      print('Invalid password');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Invalid password')),
+      );
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Login page"),
+            TextField(
+              controller: MailController,
+              decoration: InputDecoration(
+                hint: Text("Write your mail")
+              ),
+            ),
+            TextField(
+              controller: PasswordController,
+              decoration: InputDecoration(
+                hint: Text("Write your password")
+              ),
+            ),
+            ElevatedButton(onPressed: (){
+              login_verif();
+            }, child: Text("Login"))
+          ],
+        ),
+      ),
+    );
+  }
+}
